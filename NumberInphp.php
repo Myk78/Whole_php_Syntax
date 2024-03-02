f605be1cf2a58a6568c85d7011a44863d50bd2f5
Myk78 <nwbyaseen@gmail.com>
2024-03-02 16:01:28 +0500

    Number in php

---
+++ NumberInphp.php
@@ -0,0 +1,52 @@
+<?php 
+// mathmatics operation work in (B I D M A S)
+// B - barket
+// i - increment
+// D -devision
+// M - multiply
+// A - pata nh s ka bi nh pata
+
+$radius = 25;
+$pi = 3.14;
+
+// echo 2 * $radius / $pi**3;
+
+// (B I D M A S)
+echo 2 * (4+9)/3;
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+
+<section>
+    <h3 class="text-center fs-5 fst-italic"> Number Function</h3>
+        <div>
+            <p class="d-block">if age is 20.00 <b>echo floor($age)</b> so its remove the all number after decimal</p>
+            <p class="d-block">if age is 20.56 <b>echo ceil($age)</b> so its around off in next value</p>
+            <p class="d-block">if age is 20.23 <b>echo pi($age)</b> so the variable value is multiply with pi value</p>
+        </div>
+    </section>
+
+<section>
+    <h3 class="text-center fs-5 fst-italic"> shorthand Function</h3>
+        <div>
+            <p class="d-block">if age is 20 <b>echo $age += 10</b> so it's update and new age is 30</p>
+            <p class="d-block">if age is 20 <b>echo $age -= 10</b> so it's update and new age is 10</p>>
+            <p class="d-block">if age is 20 <b>echo $age *= 10</b> so it's update and new age is 40</p>
+            <p class="d-block">if age is 20 <b>echo $age += 10</b> so it's update and new age is 30</p>
+        </div>
+    </section>
+    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
