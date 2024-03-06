6647ff71ec19680730d89ffbf93bec3a6647a6a5
Myk78 <nwbyaseen@gmail.com>
2024-03-06 20:51:09 +0500

    BreakContinue in php

--- conditionstatement.php
+++ conditionstatement.php
@@ -12,12 +12,13 @@ $price = 30;
 // };
 
 $products =[
-    ['name' => 'Apple', 'price' => '30 rupees'],
-    ['name' => 'banana', 'price' => '30 rupees'],
-    ['name' => 'Ornage', 'price' => '30 rupees'],
-    ['name' => 'pine', 'price' => '300/ rupees'],
-    ['name' => 'vine', 'price' => '3000 rupees'],
+    ['name' => 'Apple', 'price' => 35],
+    ['name' => 'banana', 'price' => 32],
+    ['name' => 'orange', 'price' => 34 ],
+    ['name' => 'pine', 'price' => 300 ],
+    ['name' => 'vine', 'price' =>3000],
 ];
+
 
 // foreach ($products as $pro) {
 //     if($pro['price'] < 20 && $pro['price'] > )
@@ -44,13 +45,32 @@ $products =[
         <h3>Show the item </h3>
         <h5>their price is less then 20 and above then 10</h5>
         <?php foreach($products as $pro){ ?>
-            <?php if ($pro['price'] < 60 && $pro['price'] > 20){ ?>
+            <?php if ($pro['price'] < 600 && $pro['price'] > 20){ ?>
             <p>her that item 
             <?php
                echo $pro['name'] . '<br/>';
             } ?></p>
             
             <?php } ?>
+
+            <h4>show the item with out condition</h4>
+            <p>her it's is</p>
+            <?php foreach($products as $pro){
+                // print_r($pro);
+                if ($pro['price'] < 60 && $pro['price']>30 ) {
+                    
+                    echo $pro['name'];
+                    
+                    if ($pro['name'] == "orange") {
+                        break;
+                    };
+                    
+                }
+                } ?>
+
+
+
+            
 
     </main>
     
